d7e6cf9c29bf352e228c4940b591e17128af4c13
TheQueenTofu <148783817+TheQueenTofu@users.noreply.github.com>
2024-01-09 10:00:20 +0100

    Update Quete_voiture.py

--- Quete_voiture.py
+++ Quete_voiture.py
@@ -6,6 +6,9 @@ import matplotlib.pyplot as plt
 # Chargement des données
 donnees = pd.read_csv('https://raw.githubusercontent.com/murpi/wilddata/master/quests/cars.csv')
 
+# Print column names to check the correct name
+st.write("Column Names:", donnees.columns)
+
 # Création du widget pour sélectionner la région
 region_selector = st.selectbox('Région:', ['Toutes les régions', 'US', 'Europe', 'Japon'])
 
@@ -14,7 +17,7 @@ def update_plot(region):
     plt.figure(figsize=(12, 6))
     
     # Filtrer les données en fonction de la région sélectionnée
-    filtered_data = donnees if region == 'Toutes les régions' else donnees[donnees['Region'] == region]
+    filtered_data = donnees if region == 'Toutes les régions' else donnees[donnees['Continent'] == region]
 
     # Tracé de la distribution de chaque variable pour la région sélectionnée
     for i, col in enumerate(filtered_data.columns):
