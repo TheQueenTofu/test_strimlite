3903e3a923daff93603602cb0b31ef3725e13c82
TheQueenTofu <148783817+TheQueenTofu@users.noreply.github.com>
2024-01-09 10:14:57 +0100

    Update Quete_voiture.py

--- Quete_voiture.py
+++ Quete_voiture.py
@@ -17,25 +17,25 @@ st.title("Analyse data des voitures entre les US, l'Europe et le Japon")
 
 # Fonction de mise à jour du graphique en fonction du continent sélectionné
 def update_plot(continent):
-    plt.figure(figsize=(12, 6))
+    plt.figure(figsize=(16, 8))
     
     # Filtrer les données en fonction du continent sélectionné
     filtered_data = donnees if continent == 'Tous les continents' else donnees[donnees['continent'] == continent]
 
     # Tracé de la distribution de chaque variable pour le continent sélectionné
-    for i, col in enumerate(filtered_data.columns):
-        plt.subplot(2, len(filtered_data.columns)//2, i+1)
+    for i, col in enumerate(filtered_data.columns[:-1]):
+        plt.subplot(2, 4, i+1)
         sns.histplot(filtered_data[col], kde=True)
         plt.title(f'Distribution de {col}')
 
     # Afficher le heatmap de corrélation
-    plt.subplot(2, 2, len(filtered_data.columns) + 1)
+    plt.subplot(2, 4, 8)
     correlation_matrix = filtered_data.corr()
     sns.heatmap(correlation_matrix, annot=True, cmap="coolwarm", linewidths=.5)
     plt.title("Heatmap de Corrélation")
 
     # Tracé de nuages de points pour les paires de variables corrélées pour le continent sélectionné
-    plt.subplot(2, 2, len(filtered_data.columns) + 2)
+    plt.subplot(2, 4, 5)
     sns.pairplot(filtered_data, diag_kind='kde')
     plt.suptitle(f'Nuages de points pour les paires de variables corrélées ({continent})')
 
